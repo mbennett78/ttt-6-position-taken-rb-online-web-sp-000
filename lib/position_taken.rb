077ae606f8887bf024ee64ad9fbb5f9d9a8e7fb6
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-03-22 02:18:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,10 @@
 def position_taken?(board, position)
   taken = nil
 
-  if board[position] =="" || board[position]== " "
-    taken = false
-  else
+  if board[position] =="X" || board[position]== "O"
     taken = true
+  else 
+    taken = false
 
   end
 
