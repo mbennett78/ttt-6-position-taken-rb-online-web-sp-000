59978f5afd9fc46bc5a0b60b33b8b7329b7ef013
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-03-22 02:23:47 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+  taken = nil
+
+  if board[position] =="X" || board[position]== "O"
+    taken = true
+  else
+    taken = false
+
+  end
+
+return taken
+
+
+end
