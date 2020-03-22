c80c1a0ef178220fa8d63b54c3006e8a15277da6
modern-helper-8600 <bennett.melissa1@gmail.com>
2020-03-22 02:10:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+require_relative '../lib/position_taken.rb'
+def position_taken?(board,index)
+  if index ==" " or index=="  "
+    return false
+  else
+    return true
